c7b55890d93860137daec49d67b75250fb029bdb
Swapnil639 <swapnilpatil631997@gmail.com>
2023-06-13 23:03:26 +0530

    Added :Multiple time flipping coin store singlet combination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 
-result=$((RANDOM % 2))
+declare -A singlet
 
-if [ $result -eq 0 ]; then
-  echo "Heads"
-else
-  echo "Tails"
-fi
+heads=0
+tails=0
+
+for ((i=1; i<=100; i++))
+do
+  result=$((RANDOM % 2))
+
+  if [ $result -eq 0 ]; then
+    heads=$((heads + 1))
+    singlet[Heads]=$heads
+  else
+    tails=$((tails + 1))
+    singlet[Tails]=$tails
+  fi
+done
+
+total=$((heads + tails))
+for key in "${!singlet[@]}"
+do
+  percentage=$((100 * ${singlet[$key]} / $total))
+  echo "$key percentage: $percentage%"
+done
